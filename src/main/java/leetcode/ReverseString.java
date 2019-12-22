2f2f3200f01287faa298fbaaddef96dfa77b6219
MatiasDwek <matiasdwek@gmail.com>
2019-12-22 21:49:36 +0100

    Add reverse string exercise

---
+++ src/main/java/leetcode/ReverseString.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/reverse-string
+ */
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        char temp;
+        for (int i = 0; i < s.length / 2; i++) {
+            temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+        }
+    }
+}
